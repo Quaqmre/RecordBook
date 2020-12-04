2c6fb427d1b4c590976c9814749cf9a8ff60a21d
Süha Arıkan <suha-arikan@hotmail.com>
2020-12-04 23:56:41 +0300

    UI

---
+++ UI-React/src/utils/Alert.js
@@ -0,0 +1,17 @@
+import React, { useEffect } from "react";
+import { useToasts } from "react-toast-notifications";
+
+const Alertify = ({ content ,appearance}) => {
+  const { addToast } = useToasts();
+  let setAppearance = appearance;
+  useEffect(() => {
+    addToast(content, {
+      appearance: setAppearance === true ? "success":"error",
+      autoDismiss: true,
+    });
+  }, [addToast, content, setAppearance]);
+
+  return <div></div>;
+};
+
+export default Alertify;
